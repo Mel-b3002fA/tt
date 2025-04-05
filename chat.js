78e0bb15c0794ff589a9ca202a1a5da75dd03acc
Milena <142824252+Mel-b3002fA@users.noreply.github.com>
2025-04-05 21:56:12 +0200

    push

--- chat.js
+++ chat.js
@@ -1,4 +1,4 @@
-async function getAIResponse(userInput) {
+/* async function getAIResponse(userInput) {
     const response = await fetch("http://127.0.0.1:5000/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -7,4 +7,4 @@ async function getAIResponse(userInput) {
 
     const data = await response.json();
     return data.response;
-}
+} */
