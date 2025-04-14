9d6aa2d70b7834122162186d4245cffc1ec4732b
Milena <142824252+Mel-b3002fA@users.noreply.github.com>
2025-04-14 23:53:28 +0200

    push

--- chat.js
+++ chat.js
@@ -43,3 +43,15 @@
         });
     }
      */
+
+    fetch("https://joi-prototype.onrender.com/chat", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ message: "hi" })
+      })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
+      
