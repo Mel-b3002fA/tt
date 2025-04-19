00f4ef8286b0d91658238e00765c10ae2b7413c6
Milena <142824252+Mel-b3002fA@users.noreply.github.com>
2025-04-19 10:18:00 +0200

    push

--- server.py
+++ server.py
@@ -216,7 +216,13 @@ def chat():
 """ 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)  # Running on port 5001 (or any other available port) """
-if __name__ == '__main__':
+
+
+
+
+
+
+""" if __name__ == '__main__':
     import socket
 
     def find_free_port():
@@ -234,7 +240,13 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Or configure specifically if needed
+CORS(app)  # Or configure specifically if needed """
+
+
+
+
+
+
 
 
 """ if __name__ == '__main__':
@@ -242,3 +254,7 @@ CORS(app)  # Or configure specifically if needed
 
    app.run(debug=True, port=5050) """
 
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000, debug=True)
